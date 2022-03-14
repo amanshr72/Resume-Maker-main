f4ecddfc952faeec1afb09a5fadc8dcef6ea973d
Aman Sharma <amansh2786@gmail.com>
2022-03-14 22:58:37 +0530

    Updates

---
+++ precache-manifest.ef6ea949c4138dceb3880fb3da746f94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18642ab132d7575b30eb",
+    "url": "/Ami-Resume-Maker/static/css/main.849ac7d6.chunk.css"
+  },
+  {
+    "revision": "18642ab132d7575b30eb",
+    "url": "/Ami-Resume-Maker/static/js/main.18642ab1.chunk.js"
+  },
+  {
+    "revision": "d91c2e85d23555c225c6",
+    "url": "/Ami-Resume-Maker/static/js/1.d91c2e85.chunk.js"
+  },
+  {
+    "revision": "bcec585bb585f2e0c521",
+    "url": "/Ami-Resume-Maker/static/js/runtime~main.bcec585b.js"
+  },
+  {
+    "revision": "9d47a74b30f58b3f48ddf1e8a659715b",
+    "url": "/Ami-Resume-Maker/index.html"
+  }
+];
